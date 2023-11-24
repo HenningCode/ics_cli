13f324bffa837182a12580596dd7bd28a604d5ec
HenningCode <henningwilmer@yahoo.de>
2023-11-24 18:05:55 +0100

    refactor getting methods

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,12 +1,10 @@
-#[derive(PartialEq)]
-#[derive(Debug)]
-pub enum Return {Yes,No}
+use regex::Regex;
 
-pub fn match_input(input: &str) -> Result<Return,&'static str>{
-    match input.to_lowercase().trim(){
-        "y" => return Ok(Return::Yes),
-        "n" => return Ok(Return::No),
-        _ => return Err("Wrong Input, try again.\nPossible answers are [y/n]!"),
+pub fn confirm_input(input: &str) -> Option<bool> {
+    match input.to_lowercase().trim() {
+        "y" | "Y" | "j" | "J" => return Some(true),
+        "n" | "N" => return Some(false),
+        _ => return None,
     }
 }
 
@@ -45,11 +43,132 @@ pub fn input_int_with_message(msg: &str) -> i32 {
     }
 }
 
-pub fn parse_string_date_to_int(date: String) -> (u32,u32,i32) {
-    let d = date.as_str();
-    let day:u32 = d[..2].to_string().parse().unwrap();
-    let month:u32 = d[3..5].to_string().parse().unwrap();
-    let year:i32 = d[6..10].to_string().parse().unwrap();
-    (day,month,year)
+pub fn input_with_regex_validation(msg: &str, msg_err: &str, re: &Regex) -> String {
+    let mut date = input_string_with_message(msg);
+    loop {
+        let valid = re.is_match(&date);
+        if valid {
+            return date;
+        }
+        date = input_string_with_message(msg_err);
+    }
 }
- 
+
+pub fn parse_string_date_to_int(date: &String) -> (u32, u32, i32) {
+    let d = date.as_str();
+    let day: u32 = d[..2]
+        .to_string()
+        .parse()
+        .expect("this should already be verified ");
+    let month: u32 = d[3..5]
+        .to_string()
+        .parse()
+        .expect("this should already be verified ");
+    let year: i32 = d[6..10]
+        .to_string()
+        .parse()
+        .expect("this should already be verified ");
+    (day, month, year)
+}
+
+pub fn parse_string_time_to_int(date: &String) -> (u32, u32) {
+    let d = date.as_str();
+    let hours: u32 = d[..2]
+        .to_string()
+        .parse()
+        .expect("this should already be verified ");
+    let minutes: u32 = d[3..5]
+        .to_string()
+        .parse()
+        .expect("this should already be verified ");
+    (hours, minutes)
+}
+
+pub fn validate_in_range_u32(value: u32, min: u32, max: u32) -> bool {
+    if value <= min || value >= max {
+        return false;
+    }
+    true
+}
+
+pub fn validate_in_range_i32(value: i32, min: i32, max: i32) -> bool {
+    if value <= min || value >= max {
+        return false;
+    }
+    true
+}
+
+pub fn calc_feb_days(year: i32) -> u32 {
+    if year % 400 == 0 {
+        return 29;
+    }
+    if year % 4 == 0 && year % 100 !=0 {
+        return 28;
+    }
+    28
+}
+
+pub fn get_date() -> (u32, u32, i32) {
+    let re_date: Regex = Regex::new(r"^\d{2}\.\d{2}\.\d{4}$").unwrap();
+
+    let (mut day, mut month, mut year);
+    loop {
+        let date = input_with_regex_validation(
+            "Whats the date of the event? (dd.mm.yyyy)",
+            "Wrong input format. Try again! (dd.mm.yyyy)",
+            &re_date,
+        );
+        (day, month, year) = parse_string_date_to_int(&date);
+
+        if !validate_in_range_i32(year, 0, 3000) {
+            println!("Year is out of range (0-3000");
+            continue;
+        }
+
+        if !validate_in_range_u32(month, 1, 12) {
+            println!("Month is out of range (1-12)");
+            continue;
+        }
+        let max = match month {
+            1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
+            2 => calc_feb_days(year),
+            4 | 6 | 9 | 11 => 30,
+            _ => panic!("Should not happen"),
+        };
+
+        if !validate_in_range_u32(day, 1, max) {
+            println!("Day is out of range (1-{max})");
+            continue;
+        }
+        break;
+    }
+    (day, month, year)
+}
+
+pub fn get_event_time() -> (u32, u32) {
+    let re: Regex = Regex::new(r"^\d{2}:\d{2}").unwrap();
+
+    let (mut hours, mut minutes);
+    loop {
+        let time = input_with_regex_validation(
+            "Whats the time of the event? (hh:mm)",
+            "Wrong input format. Try again! (hh:mm)",
+            &re,
+        );
+        (hours, minutes) = parse_string_time_to_int(&time);
+
+        if !validate_in_range_u32(hours, 0, 23) {
+            println!("Month is out of range (1-12)");
+            continue;
+        }
+
+        if !validate_in_range_u32(minutes, 0, 59) {
+            println!("Day is out of range (0-59)");
+            continue;
+        }
+        break;
+    }
+    (hours, minutes)
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,49 @@
 use chrono_tz::Europe::Berlin;
 use icalendar::*;
 use std::{fs::File, io::Write};
-use regex::Regex;
 
 mod helpers;
 
 fn main() {
-    
-    let re = Regex::new(r"\d{2}\.\d{2}\.\d{4}").unwrap();
     println!("Hello do you want to create a calendar?[y/n]");
 
-    
-    loop{
+    loop {
         let mut line = String::new();
-        std::io::stdin().read_line(&mut line).expect("Couldn't read from stdin");
-        let input = helpers::match_input(&line);
+        std::io::stdin()
+            .read_line(&mut line)
+            .expect("Couldn't read from stdin");
+        let input = helpers::confirm_input(&line);
         match input {
-            Ok(v) => {
-                if v == helpers::Return::Yes { 
-                    break;
-                }
-                if v == helpers::Return::No { 
+            Some(v) => {
+                if !v {
                     println!("Okay bye");
                     return;
                 }
-            },
-            Err(e) => println!("{e}"),
+                break;
+            }
+            None => println!("Wrong Input, try again.\nPossible answers are [y/n]!"),
         }
-    }  
+    }
 
     let calendar_name = helpers::input_string_with_message("Whats the name of the calendar?");
     let number_event = helpers::input_int_with_message("How many Events do you want to input?");
 
-    let mut my_calendar = Calendar::new()
-        .name(&calendar_name).done();
+    let mut my_calendar = Calendar::new().name(&calendar_name).done();
     let file_name = format!("{calendar_name}.ical");
 
-    for _ in 0..number_event{
-
+    for _ in 0..number_event {
         let discription = helpers::input_string_with_message("Please input the name of the event");
-        let mut date = helpers::input_string_with_message("Whats the date of the event? (dd.mm.yyyy)");
-        loop {
-            let valid = re.is_match(&date);
-            if valid {
-                break;
-            }
-            date = helpers::input_string_with_message("Wrong input format. Try again! (dd.mm.yyyy)");
-        } 
-
-        let (day, month, year) = helpers::parse_string_date_to_int(date);
-
+        let (day, month, year) = helpers::get_date();
+        let (hours, minutes) = helpers::get_event_time();
         my_calendar.push(
             Event::new()
-                .starts(CalendarDateTime::from_ymd_hm_tzid(year, month, day, 20, 15, Berlin).unwrap())
+                .starts(
+                    CalendarDateTime::from_ymd_hm_tzid(year, month, day, hours, minutes, Berlin)
+                        .unwrap(),
+                )
                 .summary(&discription)
                 .done(),
         );
-
     }
 
     let file_result = File::create(file_name.clone());
@@ -65,6 +52,7 @@ fn main() {
         Err(_) => File::open(file_name).unwrap(),
     };
 
-    file.write_all(my_calendar.to_string().as_bytes()).expect("Writing did not work!");
+    file.write_all(my_calendar.to_string().as_bytes())
+        .expect("Writing did not work!");
+    println!("{my_calendar}");
 }
-
